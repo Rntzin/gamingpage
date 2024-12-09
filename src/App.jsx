0868cfdc5cee23e4b73059bd52069f4044e54045
Rntzin <kinglemoyra@gmail.com>
2024-12-09 08:13:59 -0300

    Feat: Footer and Contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Freatures from "./components/Features";
 import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <About />
       <Freatures />
       <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 };
